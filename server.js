f69b4cb24416b8fa62d3cadd8b8b6832b39675f1
Marko Klemetti <marko.klemetti@gmail.com>
2015-09-18 15:12:42 +0300

    Cleaned server conf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var morgan  = require('morgan');
 
 var server = require('./app');
 
-var port     = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 server.use(morgan('dev', {immediate: true}));
 
