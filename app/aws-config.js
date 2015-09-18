ec9dfc02a8f5d092aa4867dbd1a73d57cb70eec9
Marko Klemetti <marko.klemetti@gmail.com>
2015-09-18 11:28:39 +0300

    Add fake keys for aws

--- app/aws-config.js
+++ app/aws-config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = { 
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'yourawsaccesskey',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'yourawssecretkey',
   region: 'eu-central-1',
   bucket: 'nodejs-mongodb-api-images',
   dirname: 'images'
